ed5cafdbece9f39f65bc549bb50f50e5db55b847
Mesky <dmeskis@gmail.com>
2018-11-04 15:52:49 -0700

    Wrote test for weather api

---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'Weather API' do
+  it 'includes the current temperature and high and low temperature for the day' do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+
+    weather_data = JSON.parse(response.body)
+
+    expect(weather_data[:data]).to exist
+  end
+end
+
+# As a user,
+# when i send a GET request to the below endpoint,
+# I should send the data from the query to Google's Geocoding API to retrieve the lat and long for the city
+#
+# GET /api/v1/forecast?location=denver,co
+# Content-Type: application/json
+# Accept: application/json
+#
+# As a user,
+# when i sent a get request to the below endpoint
+# the JSON response should include the current temperature,
+# and should have a high and low temperature for the day.
+#
+# GET /api/v1/forecast?location=denver,co
+# Content-Type: application/json
+# Accept: application/json
