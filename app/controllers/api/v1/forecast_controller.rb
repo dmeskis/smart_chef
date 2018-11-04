bc5478e4ef0c0b88a9a8c3e6961e6790fb90ab27
Mesky <dmeskis@gmail.com>
2018-11-04 16:22:39 -0700

    Grabbed current temp, high temp, and low temp from darksky API

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -12,6 +12,18 @@ class Api::V1::ForecastController < ApplicationController
     parsed_response = JSON.parse(response.body)
     lat = parsed_response["results"][0]["geometry"]["location"]["lat"]
     lon = parsed_response["results"][0]["geometry"]["location"]["lng"]
+
+    darksky_conn = Faraday.new(url: "https://api.darksky.net") do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.headers['Accept'] = 'application/json'
+      f.adapter Faraday.default_adapter
+    end
+
+    response = darksky_conn.get("/forecast/#{ENV["DARKSKY_API_KEY"]}/#{lat},#{lon}")
+    parsed_response = JSON.parse(response.body)
+    temp = parsed_response["currently"]["temperature"]
+    high_temp = parsed_response["daily"]["data"][0]["temperatureHigh"]
+    low_temp = parsed_response["daily"]["data"][0]["temperatureLow"]
     binding.pry
     # /maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY"
   end
