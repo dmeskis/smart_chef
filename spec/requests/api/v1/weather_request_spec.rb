edb6b50a0fe0b480daaa8048bc021767e64bc993
Mesky <dmeskis@gmail.com>
2018-11-05 10:24:49 -0700

    Completes user story 3 by retrieving more weather data

--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -8,9 +8,36 @@ describe 'Weather API' do
     expect(response).to be_successful
 
     weather_data = JSON.parse(response.body)
-    expect(weather_data.keys).to contain_exactly('current_temperature', 'high_temp', 'low_temp')
+    expect(weather_data.keys).to contain_exactly('current_temperature',
+                                                 'high_temp',
+                                                 'low_temp',
+                                                 'current_description',
+                                                 'feels_like',
+                                                 'humidity',
+                                                 'visibility',
+                                                 'uv_index',
+                                                 'day_description',
+                                                 'night_description'
+                                                 )
   end
 end
+
+# As a user,
+# when I send a get request to the below endpoint,
+# the JSON response should include details of the weather,
+# including a description of the weather,
+# feels like (in degrees),
+# humidity (in percent),
+# visibility (in miles),
+# UV Index (e.g. 2 (low)),
+# A description of today,
+# and a description of tonight
+#
+# GET /api/v1/forecast?location=denver,co
+# Content-Type: application/json
+# Accept: application/json
+
+
 
 # As a user,
 # when i send a GET request to the below endpoint,
