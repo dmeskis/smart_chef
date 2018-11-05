23af49ee95ca822b7cc806b4010b014ae89d9dd8
Mesky <dmeskis@gmail.com>
2018-11-05 12:59:50 -0700

    Deletes commented out code

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -12,16 +12,5 @@ describe Forecast, type: :model do
     it {should validate_presence_of(:uv_index)}
     it {should validate_presence_of(:day_description)}
     it {should validate_presence_of(:night_description)}
-    # 
-    # it {should have_a(:current_temperature)}
-    # it {should have_a(:high_temperature)}
-    # it {should have_a(:low_temperature)}
-    # it {should have_a(:current_description)}
-    # it {should have_a(:feels_like)}
-    # it {should have_a(:humidity)}
-    # it {should have_a(:visibility)}
-    # it {should have_a(:uv_index)}
-    # it {should have_a(:day_description)}
-    # it {should have_a(:night_description)}
   end
 end
