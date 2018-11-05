67453bd6c53f21b4406749ed3be0dec065360de4
Mesky <dmeskis@gmail.com>
2018-11-05 15:31:23 -0700

    Craptastic refactoringg

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -2,17 +2,5 @@ require 'rails_helper'
 
 describe Forecast, type: :model do
   describe 'it has attributes' do
-    # 
-    # it {should validate_presence_of(:current_temperature)}
-    # it {should validate_presence_of(:high_temperature)}
-    # it {should validate_presence_of(:low_temperature)}
-    # it {should validate_presence_of(:current_description)}
-    # it {should validate_presence_of(:feels_like)}
-    # it {should validate_presence_of(:humidity)}
-    # it {should validate_presence_of(:visibility)}
-    # it {should validate_presence_of(:uv_index)}
-    # it {should validate_presence_of(:day_description)}
-    # it {should validate_presence_of(:night_description)}
-    # it {should validate_presence_of(:weekly_forecast)}
   end
 end
