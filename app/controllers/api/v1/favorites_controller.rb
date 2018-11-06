fd0df6cb3de47920bfcb5ad881f8e254199399d9
Mesky <dmeskis@gmail.com>
2018-11-06 15:59:28 -0700

    Working on favorites controllers

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
+    if favorites_params[:api_key]
+      user = User.find_by(api_key: favorites_params[:api_key])
+      if user != nil
+        user.favorites << favorites_params[:location]
+        render json: user.favorites
+      else
+        render json: {errors: "error"}, status: 401
+      end
+    else
+      render json: {errors: "error"}, status: 401
+    end
   end
+
+  private
+
+    def favorites_params
+      params.permit(:location, :api_key)
+    end
 end
