bd48422e40ba87d6bb1378fec82517aaa86a355b
Mesky <dmeskis@gmail.com>
2018-11-06 13:22:53 -0700

    Remove conditional in background controller'

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     coordinates = GoogleGeocodingService.new.get_coordinates(params[:location])
     response = FlickrService.new.get_background_images(coordinates)
-    if response[:stat] == "ok"
-      render json: response
-    else
-      render status: 400
-    end
+    render json: response
   end
 
 end
