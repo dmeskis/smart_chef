45b41ffb17772a61f09d5798a7133f3bc8a05217
Mesky <dmeskis@gmail.com>
2018-11-06 13:20:33 -0700

    background returns image data we need

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -6,18 +6,6 @@ describe 'Background API' do
 
     expect(response).to be_successful
     image_data = JSON.parse(response.body)
-    binding.pry
+    expect(image_data.keys).to contain_exactly('photos', 'stat')
   end
 end
-
-# As a user,
-# when i visit the weather page,
-# the background image is randomly pulled from an imaging site using the below endpoint in our app.
-#
-# The image must be limited to the city being viewed.
-#
-# APIs to user: Flickr or photos.getInfo
-#
-# Stretch goal: Write a rake task that caches photos for the 50 largest U.S. cities to improve page load times (Throw this in another user story)
-#
-# GET /api/v1/backgrounds?location=denver,co
