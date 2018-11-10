c37bb6fc3f44fec59d6f52a0db2f772f46ca00cd
Mesky <dmeskis@gmail.com>
2018-11-10 12:06:57 -0700

    Wrote test for deleting a favorite

--- spec/requests/api/v1/favoriting_locations_spec.rb
+++ spec/requests/api/v1/favoriting_locations_spec.rb
@@ -25,4 +25,22 @@ describe 'favoriting locations API' do
     expect(data["data"][0]["type"]).to eq("favorite")
     expect(data["data"][0]["attributes"].keys).to contain_exactly('id', 'location', 'current_weather')
   end
+  it 'allows a user to delete a favorite' do
+    user = create(:user)
+    user.cities.create(name: "Denver", state: "CO")
+    delete "/api/v1/favorites?location=Denver,+CO&api_key=#{user.api_key}"
+    expect(response).to be_successful
+    expect(user.cities.count).to eq(1)
+  end
 end
+
+# DELETE /api/v1/favorites
+# Content-Type: application/json
+# Accept: application/json
+#
+# body:
+#
+# {
+#   "location": "Denver, CO", # If you decide to store cities in your database you can send an id if you prefer
+#   "api_key": "jgn983hy48thw9begh98h4539h4"
+# }
